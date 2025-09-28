247f30f0bf10b676c703038790c20b3abe0357cd
Mr Khono <mrkhono@macbookpro.home>
2025-09-28 23:32:48 +0000

    + update

--- components/profil/sidebar.jsx
+++ components/profil/sidebar.jsx
@@ -79,8 +79,8 @@ export function Sidebar(){
     { href: '/user-profil/profil-settings',  label: 'Profil', active: true },
     { href: '/user-profil/mes-sessions',  label: 'Mes Sessions', active: false },
     { href: '/account/wallet', label: 'Mes crédits', active: false },
-    { href: '/account/address', label: 'Address', active: false },
-    { href: '/account/notifications',  label: 'Notification', active: false },
+    // { href: '/account/address', label: 'Address', active: false },
+    // { href: '/account/notifications',  label: 'Notification', active: false },
     { href: '/', label: 'Déconnexion', active: false }
   ];
     return (
