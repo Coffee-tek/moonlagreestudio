655380a5a2dcdf379cc060889f378f56e6945033
Ophélie ANDZALA <ondzalaoph@gmail.com>
2025-12-06 02:29:46 +0000

    derniere mise a jour

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.jsdelivr.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  },
-  serverExternalPackages: ["@node-rs/argon2"],
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'cdn.jsdelivr.net',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'avatars.githubusercontent.com',
+  //     },
+  //   ],
+  // },
+  // serverExternalPackages: ["@node-rs/argon2"],
+  
 };
 
 export default nextConfig;
