9d060b9022faa3223bda4f21f949fa5361e27781
Ophélie ANDZALA <ondzalaoph@gmail.com>
2025-12-18 01:35:58 +0000

    derniere mise a jour

--- src/components/user/mes-credits/TransactionCard.jsx
+++ src/components/user/mes-credits/TransactionCard.jsx
@@ -11,38 +11,38 @@ export default function TransactionCard({ transaction }) {
 
   return (
     <div className="col p-1">
-      <div className="d-flex align-items-center justify-content-between bg-white border px-4 py-4 rounded-4 shadow-sm">
-        <div className="w-75">
-          <div className="d-flex align-items-center gap-3">
+      <div className="d-flex flex-column flex-md-row align-items-start align-items-md-center justify-content-between bg-white border px-3 py-3 rounded-4 shadow-sm">
+        <div className="w-100 w-md-75 mb-2 mb-md-0">
+          <div className="d-flex align-items-start align-items-md-center gap-3 flex-wrap">
             <i
               className={`ri-${isPositive ? "add" : "subtract"}-line text-muted fs-5`}
             ></i>
             <div className="lh-sm">
               <h4
-                className={`fw-bold ${
-                  isPositive ? "text-success" : "text-danger"
-                } mb-2`}
+                className={`fw-bold ${isPositive ? "text-success" : "text-danger"
+                  } mb-1`}
               >
                 {transaction.type}
               </h4>
-              <p className="text-truncate mb-2 small text-muted">
+              <p className="text-truncate mb-1 small text-muted">
                 {transaction.description || "Aucune description"}
               </p>
               <small className="text-muted">{formattedDate}</small>
             </div>
           </div>
         </div>
-        <div className="ms-auto d-flex align-items-center gap-3 text-center small">
+
+        <div className="d-flex align-items-center gap-3 text-center small mt-2 mt-md-0">
           <span
-            className={`${
-              isPositive ? "text-success" : "text-danger"
-            } fw-bold h5 m-0`}
+            className={`${isPositive ? "text-success" : "text-danger"
+              } fw-bold h5 m-0`}
           >
             {isPositive ? "+" : "-"}
             {transaction.montant} crédits
           </span>
         </div>
       </div>
+
     </div>
   );
 }
